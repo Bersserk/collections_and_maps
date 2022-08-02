0910f22ddbc0410afb946f845985b2dd03608204
BersDeveloper <bers.developer@gmail.com>
2022-08-02 18:59:48 +0200

    Cleared Main branch

--- app/src/main/java/com/example/collections_and_maps/MainActivity.java
+++ app/src/main/java/com/example/collections_and_maps/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.collections_and_maps;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
-        viewPager2.setAdapter(adapter);
-
-        new TabLayoutMediator(tabLayout, viewPager2,
-                (tab, position) -> tab.setText(position==0?"Collections":"Maps")).attach();
     }
 }
