f3420743c2125d554cca0be8917961f8bdbce9e2
Bersserk <bers.developer@gmail.com>
2022-09-30 20:48:33 +0200

    25. # output into recycler has done

--- app/src/main/java/com/example/collections_and_maps/ui/benchmark/CollectionsPagerFragment.java
+++ app/src/main/java/com/example/collections_and_maps/ui/benchmark/CollectionsPagerFragment.java
@@ -1,8 +1,10 @@
 package com.example.collections_and_maps.ui.benchmark;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.collections_and_maps.R;
 
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CollectionsPagerFragment extends BaseFragment {
 
     BenchmarksAdapter adapter;
+    Handler mHandler;
 
     public static CollectionsPagerFragment newInstance(String param1) {
         CollectionsPagerFragment fragment = new CollectionsPagerFragment();
@@ -28,6 +33,8 @@ public class CollectionsPagerFragment extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mHandler = new Handler();
 //        Resources res = this.requireActivity().getResources();
 //        listNamesMainItem = res.getStringArray(R.array.collections);
 //        listNamesItem = res.getStringArray(R.array.collections_item);
@@ -57,7 +64,7 @@ public class CollectionsPagerFragment extends BaseFragment {
     public void checkInputCorrectly() {
         super.checkInputCorrectly();
 
-        ArrayList <> resultList  = new ArrayList<>();
+        ArrayList<String> resultList = new ArrayList<>();
         // button was pushed, next we are initialisation all views
 //        for (int s = 3; s < resultList.size(); s++) {
 //            String nameLine = resultList.get(s).toString();
@@ -66,79 +73,49 @@ public class CollectionsPagerFragment extends BaseFragment {
 //            resultList.set(++s, new MyCopyOnWriteArrayList(k, nameLine).getResult());
 //        }
 
-        // нам нужно запустить цикл потока, где внутри будут вызываться методы на расчет...
-        // ...и выдавая результаты обновлять подаваемый в адаптер список
-
-        JThread t= new JThread("JThread ");
-        t.start();
-        t.notify();
-
-        try{
-            t.join();
+        for (int s = 0; s < 7; s++) {
+            resultList.add(String.valueOf(s));
         }
-        catch(InterruptedException e){
-
-            System.out.printf("%s has been interrupted", t.getName());
-        }
-
-
 
         for (int s = 0; s < 7; s++) {
-            resultList.add("прив".startNewThread(10));
-            resultList.add(startNewThread(1));
-            resultList.add(startNewThread(5));
-            resultList.add(startNewThread(7));
-            resultList.add(startNewThread(20));
-            resultList.add(startNewThread(8));
-            resultList.add(startNewThread(6));
-
-//            resultList.set(++s, new MyLinkedList(k, nameLine).getResult());
-//            resultList.set(++s, new MyCopyOnWriteArrayList(k, nameLine).getResult());
+            beginNewThread(s, new Random().nextInt(10000), resultList);
         }
 
-        adapter = new BenchmarksAdapter(this, resultList);
-        getRecycler().setAdapter(adapter);
     }
 
-    @Override
-    public void onClick(View view) {
-        checkInputCorrectly();
-    }
-
-    public static void startNewThread(int sec) {
-
-        new Thread(new Runnable() {
+    public void beginNewThread(int i, int i1, ArrayList resultList) {
+        Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(sec * 1000);
 
+                    Thread.sleep(i1);
 
+                    resultList.set(i, "" +i+i+i+i);
+                    reWrite(resultList);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        }).start();
-
-
+        });
+        t.start();
     }
 
-}
-
-class JThread extends Thread {
-
-    JThread(String name){
-        super(name);
+    public void reWrite (ArrayList resultList) {
+        adapter = new BenchmarksAdapter(this, resultList);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                getRecycler().setAdapter(adapter);
+            }
+        });
     }
 
-    public void run(){
 
-        try{
-            Thread.sleep(500);
-        }
-        catch(InterruptedException e){
-            System.out.println("Thread has been interrupted");
-        }
 
+    @Override
+    public void onClick(View view) {
+            checkInputCorrectly();
     }
 }
+
