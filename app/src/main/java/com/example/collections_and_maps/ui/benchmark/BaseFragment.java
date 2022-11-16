486d817ae99f84816283cb68c842b13a9e12f363
Bersserk <bers.developer@gmail.com>
2022-11-16 18:12:10 +0100

    55. # Onclick with Exceptions was done

--- app/src/main/java/com/example/collections_and_maps/ui/benchmark/BaseFragment.java
+++ app/src/main/java/com/example/collections_and_maps/ui/benchmark/BaseFragment.java
@@ -1,7 +1,10 @@
 package com.example.collections_and_maps.ui.benchmark;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,17 +22,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.collections_and_maps.R;
 import com.example.collections_and_maps.models.benchmarks.Compute;
 import com.example.collections_and_maps.models.benchmarks.Item;
-import com.example.collections_and_maps.models.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseFragment extends Fragment implements View.OnClickListener {
-    private Handler mHandler = new Handler();
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private InputData inputData;
+    Compute compute;
 
     protected static final String TYPE_BENCHMARK = "type";
 
-    private  BenchmarksAdapter adapter = new BenchmarksAdapter();
+    private BenchmarksAdapter adapter = new BenchmarksAdapter();
     protected EditText collectionSize;
     boolean yes = false;
 
@@ -41,7 +45,8 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        collectionSize = view.findViewById(R.id.collectionSize);
+        EditText collectionSize = view.findViewById(R.id.collectionSize);
+        inputData = new InputData(collectionSize, this.getContext());
         Button calcButton = view.findViewById(R.id.calcButton);
         calcButton.setOnClickListener(this);
 
@@ -55,27 +60,16 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
         listRecycler.setHasFixedSize(true);
         listRecycler.setLayoutManager(gridLayoutManager);
 
-        List<Item> list = this.createTemplateList();
-        Logger.mlog("list", "templateList", list);
-        adapter.submitList(list);
+        adapter.submitList(this.createTemplateList());
         listRecycler.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View view) {
-        int sizeList = getSizeList();
-
-
-                    List<Item> list = this.createTemplateList();
-                    Compute compute = new Compute(adapter, sizeList, mHandler);
-        if (yes) {
-            compute.clearList(list);
-//            compute.stop();
-            compute.updateUI();
-
+        if (inputData.isNoEqual() || inputData.getInputData() == 0) {
+            compute = new Compute(adapter, inputData.getInputData());
         } else {
-            yes = true;
-            compute.updateUI();
+            Toast.makeText(getContext(), R.string.OtherValue, Toast.LENGTH_LONG).show();
         }
     }
 
@@ -104,22 +98,6 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
         return templateList;
     }
 
-    public int getSizeList() {
-        int size;
-        try {
-            size = Integer.parseInt(collectionSize.getText().toString());
-            if (size > 0) {
-                return size;
-            } else {
-                Toast.makeText(getContext(), R.string.OverZero, Toast.LENGTH_LONG).show();
-                return 0;
-            }
-        } catch (NumberFormatException e) {
-            Toast.makeText(getContext(), R.string.ToastsText, Toast.LENGTH_LONG).show();
-            return 0;
-        }
-    }
-
 
     // we will need this block later ***
 //    public static CollectionsPagerFragment newInstance(String fragmentData) {
@@ -130,6 +108,51 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
 //        return fragment;
 //    }
 
+}
+
+class InputData {
+    public int getInputData() {
+        return inputData;
+    }
+
+    int inputData;
+    EditText fieldInputData;
+    Context context;
+
+    public InputData(EditText fieldInputData, Context context) {
+        this.fieldInputData = fieldInputData;
+        this.context = context;
+    }
+
+    public int set() {
+        String text = fieldInputData.getText().toString();
+        try {
+            int newInputData = Integer.parseInt(text);
+            if (newInputData > 0) {
+                return newInputData;
+            } else {
+                Toast.makeText(context, R.string.OverZero, Toast.LENGTH_LONG).show();
+                return 0;
+            }
+        } catch (NumberFormatException e) {
+            if (text.toCharArray().length > 0) {
+                Toast.makeText(context, R.string.ToastsText, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, R.string.NeedAnyValue, Toast.LENGTH_LONG).show();
+            }
+            return 0;
+        }
+    }
+
+    public boolean isNoEqual() {
+        int temp = set();
+        if (inputData != temp) {
+            inputData = temp;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
