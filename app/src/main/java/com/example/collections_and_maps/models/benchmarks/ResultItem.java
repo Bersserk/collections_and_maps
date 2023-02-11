6169021f4e87e930371cf1d9fe8e64f72193b560
Bersserk <bers.developer@gmail.com>
2023-02-11 20:17:14 +0100

    125. # Resolved some comments

---
+++ app/src/main/java/com/example/collections_and_maps/models/benchmarks/ResultItem.java
@@ -0,0 +1,29 @@
+package com.example.collections_and_maps.models.benchmarks;
+
+import com.example.collections_and_maps.R;
+
+public class ResultItem {
+    public static final double EMPTY = -1.0;
+    public final int headerText;
+    public final  int methodName;
+    public final  boolean progressVisible;
+    public final  double timing;
+    public final  int nameForHeader;
+
+    public ResultItem(int headerText, int methodName, double timing, boolean progressVisible) {
+        this.headerText = headerText;
+        this.methodName = methodName;
+        this.timing = timing;
+        this.progressVisible = progressVisible;
+        this.nameForHeader = headerText == R.string.empty ? methodName : headerText;
+    }
+
+    public boolean isHeader() {
+        return headerText == R.string.empty || methodName == R.string.empty;
+    }
+
+    public boolean isResult() {
+        return timing > EMPTY;
+    }
+
+}
