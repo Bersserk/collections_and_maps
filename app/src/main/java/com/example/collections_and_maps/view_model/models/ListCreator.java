cce2dcb1c2fcadbb8b61ce95545e2324220901ee
Bersserk <bers.developer@gmail.com>
2023-02-13 19:08:17 +0100

    Revert "126. # Fix on review --- [merge this class with MapsComputeTime and CollectionsComputeTime (according to type of data)]"
    This reverts commit 25b5cd41c3c81d1d6174b78d3baf345863d23553.

--- app/src/main/java/com/example/collections_and_maps/view_model/models/ListCreator.java
+++ app/src/main/java/com/example/collections_and_maps/view_model/models/ListCreator.java
@@ -3,7 +3,7 @@ package com.example.collections_and_maps.view_model.models;
 import static com.example.collections_and_maps.models.benchmarks.ResultItem.EMPTY;
 
 import com.example.collections_and_maps.R;
-import com.example.collections_and_maps.models.benchmarks.DataInflater;
+import com.example.collections_and_maps.models.benchmarks.DataFilter;
 import com.example.collections_and_maps.models.benchmarks.ResultItem;
 
 import java.util.ArrayList;
@@ -12,16 +12,16 @@ import java.util.List;
 public class ListCreator {
     public final List<ResultItem> itemsList;
 
-    public ListCreator(DataInflater dataInflater, boolean itemAnimated) {
+    public ListCreator(DataFilter dataFilter, boolean itemAnimated) {
         itemsList = new ArrayList<>();
 
-        for (int itemOfListHead : dataInflater.getListHeadsId()) {
+        for (int itemOfListHead : dataFilter.getListHeadsId()) {
             itemsList.add(new ResultItem(itemOfListHead, R.string.empty, EMPTY, false));
         }
 
-        for (int methodsID : dataInflater.getListMethodsId()) {
+        for (int methodsID : dataFilter.getListMethodsId()) {
             itemsList.add(new ResultItem(R.string.empty, methodsID, EMPTY, false));
-            for (int headsID : dataInflater.getListHeadsId()) {
+            for (int headsID : dataFilter.getListHeadsId()) {
                 itemsList.add(new ResultItem(headsID, methodsID, EMPTY, itemAnimated));
             }
         }
