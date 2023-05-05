2893b4aec3486e9a446e025f281e720f91bb6f05
Bersserk <bers.developer@gmail.com>
2023-05-05 22:16:56 +0200

    14. # preliminary decision about Inject for BenchmarkType

--- app/src/main/java/com/example/collections_and_maps/models/AppComponent.java
+++ app/src/main/java/com/example/collections_and_maps/models/AppComponent.java
@@ -1,10 +1,11 @@
 package com.example.collections_and_maps.models;
 
+import com.example.collections_and_maps.ui.benchmark.BenchmarkTypeModule;
 import com.example.collections_and_maps.ui.benchmark.BenchmarkViewModelFactory;
 
 import dagger.Component;
 
-@Component(modules = {AppModule.class})
+@Component(modules = {AppModule.class, BenchmarkTypeModule.class})
 public interface AppComponent {
     void inject(BenchmarkViewModelFactory benchmarkViewModelFactory);
 }
--- app/src/main/java/com/example/collections_and_maps/ui/benchmark/BenchmarkViewModelFactory.java
+++ app/src/main/java/com/example/collections_and_maps/ui/benchmark/BenchmarkViewModelFactory.java
@@ -7,13 +7,15 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.collections_and_maps.App;
 import com.example.collections_and_maps.R;
 import com.example.collections_and_maps.models.benchmarks.Benchmark;
+import com.example.collections_and_maps.models.benchmarks.BenchmarkType;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 public class BenchmarkViewModelFactory extends ViewModelProvider.NewInstanceFactory {
 
-    private final Benchmark benchmark;
+    @Inject
+    BenchmarkType benchmarkType;
 
     @Inject
     @Named("collection")
@@ -26,14 +28,14 @@ public class BenchmarkViewModelFactory extends ViewModelProvider.NewInstanceFact
     public BenchmarkViewModelFactory(int benchmarkType) {
 
         new App().getAppComponent().inject(this);
+        this.benchmarkType.setType(benchmarkType);
 
-        benchmark = benchmarkType == R.string.Collections ? collectionsBenchmark : mapsBenchmark;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     @NonNull
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-        return (T) new BenchmarkViewModel(benchmark);
+        return (T) new BenchmarkViewModel(benchmarkType.getType() == R.string.Collections ? collectionsBenchmark : mapsBenchmark);
     }
 }
