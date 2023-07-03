5a3bb26f306501ca9215c5d479a878435d927b32
Bersserk <bers.developer@gmail.com>
2023-07-03 11:15:51 +0200

    in processing...

--- app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/ui/BenchmarkFragmentTest.java
+++ app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/ui/BenchmarkFragmentTest.java
@@ -1,21 +1,37 @@
 package com.example.collections_and_maps.ui.benchmark.ui;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.view.View;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.collections_and_maps.R;
+import com.example.collections_and_maps.models.benchmarks.MapsBenchmark;
+import com.example.collections_and_maps.models.benchmarks.ResultItem;
 import com.example.collections_and_maps.ui.benchmark.Rule;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 //@LargeTest
@@ -26,35 +42,60 @@ public class BenchmarkFragmentTest extends Rule {
         // Swipe to right
         onView(withId(R.id.view_pager2)).perform(ViewActions.swipeRight());
         // Check name after swipe
-        onView(withContentDescription(R.string.Maps)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withContentDescription(R.string.Maps)).check(matches(isDisplayed()));
 
         // Swipe to left
         onView(withId(R.id.view_pager2)).perform(ViewActions.swipeLeft());
         // Check name after swipe
-        onView(withContentDescription(R.string.Collections)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withContentDescription(R.string.Collections)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test_tabs_onClick() {
         onView(withContentDescription(R.string.Collections))
                 .perform(ViewActions.click())
-                .check(ViewAssertions.matches(isDisplayed()));
+                .check(matches(isDisplayed()));
         onView(withContentDescription(R.string.Maps))
                 .perform(ViewActions.click())
-                .check(ViewAssertions.matches(isDisplayed()));
+                .check(matches(isDisplayed()));
     }
 
     @Test
     public void test_fragment_isVisibility() {
-        // Ожидаем, что фрагмент отображается
+
+        List<ResultItem> expectedList = new MapsBenchmark().getItemsList(false);
+        int expectedItemCount = expectedList.size();
+
         onView(withId(R.id.recyclerLayoutItems))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+                .check(matches(hasMinimumChildCount(1)));
+
+//        int i = 0;
+
+        onView(withId(R.id.recyclerLayoutItems))
+                .perform(RecyclerViewActions.scrollToPosition(0))
+//                .check(matches(hasDescendant(withText(R.string.ArrayList))));
+                .check(matches(hasDescendant(withText(expectedList.get(0).nameForHeader))));
+
     }
 
     @Test
-    public void test_editText_isVisibility() {
-        onView(withId(R.id.calcButton)).check(ViewAssertions.matches(isDisplayed()));
-        onView(withId(R.id.calcButton)).check(ViewAssertions.matches(withId(R.string.calcButtonStart)));
-        onView(withId(R.id.calcButton)).perform(ViewActions.click()).check(ViewAssertions.matches(withId(R.string.calcButtonStop)));
+    public void test_editText() {
+        ViewInteraction inputField = onView(withId(R.id.inputField));
+        inputField.check(matches(Matchers.allOf(isDisplayed(), isFocusable())));
+        inputField.perform(ViewActions.typeText("1000"));
     }
+
+    @Test
+    public void test_button() {
+        ViewInteraction calcButton = onView(withId(R.id.calcButton));
+        calcButton.check(matches(Matchers.allOf(isDisplayed(), isClickable())));
+        calcButton.perform(ViewActions.click())
+                .check(matches(Matchers
+                        .anyOf(withText(R.string.calcButtonStart), withText(R.string.calcButtonStop))));
+        Espresso.closeSoftKeyboard();
+    }
+
+//    @Test
+//    public void test_
+
 }
