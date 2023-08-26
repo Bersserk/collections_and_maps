11633ff9ac3e48732204f5cee529610896e1a353
Bersserk <bers.developer@gmail.com>
2023-08-26 09:48:56 +0200

    Corrections by comments

--- app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/BenchmarkRuleTest.java
+++ app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/BenchmarkRuleTest.java
@@ -1,16 +1,37 @@
 package com.example.collections_and_maps.ui.benchmark;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withAlpha;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.app.Application;
+import android.view.View;
 
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.collections_and_maps.App;
 import com.example.collections_and_maps.R;
+import com.example.collections_and_maps.models.AppComponent;
+import com.example.collections_and_maps.models.DaggerAppComponent;
+import com.example.collections_and_maps.models.benchmarks.CollectionsBenchmark;
+import com.example.collections_and_maps.models.benchmarks.MapsBenchmark;
+import com.example.collections_and_maps.models.benchmarks.ResultItem;
 import com.example.collections_and_maps.ui.MainActivity;
+import com.example.collections_and_maps.ui.benchmark.models.AppModuleTest;
+import com.example.collections_and_maps.ui.benchmark.ui.AtPositionMatcher;
 
-import org.junit.runner.RunWith;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
 
-@RunWith(AndroidJUnit4.class)
+import java.util.List;
+
 public class BenchmarkRuleTest extends Application {
 
     protected static final int COLLECTIONS = R.string.Collections;
@@ -21,12 +42,58 @@ public class BenchmarkRuleTest extends Application {
     protected static final String EMPTY = "";
     protected static final String ANY = "any";
 
-
-
     protected final int[] TABS_NAMES = {COLLECTIONS, MAPS};
 
     @org.junit.Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void set() {
+        AppComponent appComponent =
+                DaggerAppComponent.builder().appModule(new AppModuleTest()).build();
+        App.getInstance().setAppComponent(appComponent);
+    }
+
+    protected void checkEachItem(
+            int delayBeforeMillis, int list, String textOfItem, String alphaOfItem
+    ) {
+        final List<ResultItem> initiatedList = (list == COLLECTIONS)
+                ? new CollectionsBenchmark().getItemsList(true)
+                : new MapsBenchmark().getItemsList(true);
+
+        ViewInteraction onRecyclerItems = onView(withId(R.id.recyclerLayoutItems));
+        delay(delayBeforeMillis);
+
+        final Matcher<View> text;
+        final Matcher<View> alpha;
+        if (ANY.equals(textOfItem) && ANY.equals(alphaOfItem)) {
+            text = Matchers.anyOf(withSubstring(INPUTTED_VALUE), withSubstring(EMPTY));
+            alpha = Matchers.anyOf(withAlpha(1F), withAlpha(0F));
+        } else {
+            text = withSubstring(textOfItem);
+            alpha = withAlpha(Float.parseFloat(alphaOfItem));
+        }
+
+        for (int i = 0; i < initiatedList.size(); i++) {
+            final ResultItem item = initiatedList.get(i);
+            if (item.isHeader()) {
+                onRecyclerItems.perform(RecyclerViewActions.scrollToPosition(i))
+                        .check(matches(AtPositionMatcher.atPosition(i, hasDescendant(withText(item.nameForHeader)))));
+            } else {
+                onRecyclerItems.perform(RecyclerViewActions.scrollToPosition(i))
+                        .check(matches(AtPositionMatcher.atPosition(i, hasDescendant(text))));
+                onRecyclerItems.check(matches(AtPositionMatcher.atPosition(i, hasDescendant(alpha))));
+            }
+        }
+    }
+
+    protected void delay(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/TestSuite.java
+++ app/src/androidTest/java/com/example/collections_and_maps/ui/benchmark/TestSuite.java
@@ -1,7 +1,8 @@
 package com.example.collections_and_maps.ui.benchmark;
 
-import com.example.collections_and_maps.ui.benchmark.ui.BenchmarkFragmentTestTest;
+import com.example.collections_and_maps.ui.benchmark.ui.CollectionFragmentTest;
 import com.example.collections_and_maps.ui.benchmark.ui.MainActivityTest;
+import com.example.collections_and_maps.ui.benchmark.ui.MapFragmentTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,7 +10,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         MainActivityTest.class,
-        BenchmarkFragmentTestTest.class
+        CollectionFragmentTest.class,
+        MapFragmentTest.class
 })
 public class TestSuite {
 }
